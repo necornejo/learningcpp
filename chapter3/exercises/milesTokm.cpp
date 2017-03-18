07d127a2dc8f480b46c3ce30213e13cfe5db728c
Adrian Cornejo <neadcornejo@gmail.com>
2017-03-17 23:17:58 -0500

    Avance

--- chapter3/exercises/milesTokm.cpp
+++ chapter3/exercises/milesTokm.cpp
@@ -3,5 +3,11 @@ using namespace std;
 
 int main()
 {
+  const double km_per_mile = 1.609;
+  double mile;
+  cout << "Te ayudare a convertir de millas a kilometros" << endl;
+  cout << "Ingresa las millas a convertir y presionar ENTER" << endl,
+  cin >> mile;
+  cout << mile << " en km es equivalente a..." << mile*km_per_mile << endl;
   return 0;
 }
---
+++ chapter3/exercises/twoNumbers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int numa,numb;
+  cout << "Ingresa 2 numeros !!! y presiona ENTER" << endl;
+  cin >> numa >> numb;
+  if (numa>numb)
+  {
+    cout << numa << " es mayor a " << numb << endl;
+  }
+  else
+  {
+    cout << numb << " es mayor a " << numa << endl;
+  }
+  cout << numa << "+" << numb << "=" << numa+numb << endl;
+  cout << numa << "*" << numb << "=" << numa*numb << endl;
+  cout << "El promedio de ambos numeros es " << (numa+numb)/2 << endl;
+  return 0;
+
+}
